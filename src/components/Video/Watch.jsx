ff24657c31a4ec687c6d73083b4118c54413a259
R4V3NSH4D0W <88871970+R4V3NSH4D0W@users.noreply.github.com>
2023-05-09 23:51:40 +0545

    fixed watch component

--- src/components/Video/Watch.jsx
+++ src/components/Video/Watch.jsx
@@ -5,15 +5,33 @@ import loading from '../../assets/loading.gif';
 import { Link } from 'react-router-dom';
 const Watch = () => {
   const { id } = useParams();
+  const updatedUrl = id.replace(/-episode-\d+/, '');
+  console.log(updatedUrl)
   const location = useLocation();
   const navigate=useNavigate();
-  const episodeList = location?.state?.anime;
-  console.log(episodeList?.episodes);
+  // const episodeList = location?.state?.anime;
+  // console.log(episodeList?.episodes);
+  const [episodelist,setEpisodeList]=useState([]);
   const [ep, setEpisodes] = useState([]);
   const [selectedServer, setSelectedServer] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   console.log(ep)
- 
+ useEffect(()=>{
+  const fetchEpisodes=async()=>{
+    const url=`https://api.consumet.org/anime/gogoanime/info/${updatedUrl}`;
+    try{
+        const {data}=await axios.get(url);
+        setEpisodeList(data);
+        setIsLoading(false);
+
+    }catch(error){
+        console.log(error)
+        setIsLoading(false);
+    }
+}
+fetchEpisodes();
+ },[updatedUrl])
+ console.log(episodelist)
   useEffect(() => {
     const fetchData = async () => {
       const url = `https://api.consumet.org/anime/gogoanime/servers/${id}`;
@@ -57,18 +75,18 @@ const changeEpisode=(episodeID)=>{
     <>
     <div className="flex flex-col mt-[4rem] md:flex-row">
   <div className="w-full md:w-1/5 order-2 lg:order-2   pt-4">
-  <span className='font-bold text-xl'>List of Episodes:</span>
-  <div>
-  {episodeList?.episodes?.map((list) => (
-      <button key={list.id}  onClick={()=>{
-        changeEpisode(list.id)
-      }}>
-        {list.number}
+  <span className=' font-black text-xl lg:text-2xl'>List of Episodes:</span>
+  <div className='mt-4 w-full text-white'>
+  {episodelist?.episodes?.map((list, index) => (
+    <div key={list.id} className={`${index % 2 === 0 ? 'bg-gray-400 hover:bg-gray-500' : 'bg-gray-700 hover:bg-gray-800'} text-white text-md lg:text-sm flex justify-center h-10`}>
+      <button onClick={() => changeEpisode(list.id)}>
+      {list.id.replace(/-/g, ' ')}
       </button>
+    </div>
   ))}
   </div>
 </div>
-  <div className="w-full md:w-3/5 order-1 lg:order-2 bg-gray-300">
+  <div className="w-full md:w-3/5 order-1 lg:order-2">
   <iframe
       src={selectedServer}
       title="Selected Server"
@@ -91,9 +109,19 @@ const changeEpisode=(episodeID)=>{
       ))}
     </div>
   </div>
-  <div className="w-full md:w-1/5 order-3 lg:order-3 bg-gray-400">
-    Container C
+  <div className="w-full md:w-1/5 order-3 lg:order-3">
+  <div className="flex flex-row lg:flex-col">
+    <img src={episodelist?.image} className="p-2 h-[15rem] lg:h-[23rem]" />
+    <div className="flex flex-col">
+      <span className="p-2 font-bold text-xl">{episodelist?.title}</span>
+      <div className="p-2 font-bold text-sm max-h-[12rem] overflow-y-scroll scrollbar-hide ">
+        {episodelist?.description}
+      </div>
+    </div>
   </div>
+</div>
+
+  
 </div>
    
 
